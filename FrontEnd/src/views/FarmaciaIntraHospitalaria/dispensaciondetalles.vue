9e19905d6f1c0965646c01f725446332a10bc05f
“Cristian <210180@utxicotepec.edu.mx>
2024-04-15 18:54:53 -0600

    Prueba1 de conexion con la API en la vista de dispensaciondetalles

--- FrontEnd/src/views/FarmaciaIntraHospitalaria/dispensaciondetalles.vue
+++ FrontEnd/src/views/FarmaciaIntraHospitalaria/dispensaciondetalles.vue
@@ -1,6 +1,5 @@
 <template>
   <b-container fluid>
-      
     <b-row>
       <b-col md="12">
         <iq-card>
@@ -8,34 +7,51 @@
             <h4 class="card-title">Detalles de Dispensacion</h4>
           </template>
           <template v-slot:body>
-           
-            <div class="table-responsive">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th v-for="data in columns" :key="data.field" :class="data.headerClass">
-                      {{ data.label }}
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(item, index) in rows" :key="index">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.personalMedicoId }}</td>
-                    <td>{{ item.recetaId }}</td>
-                    <td>{{ item.nombreGenerico }}</td>
-                    <td>{{ item.nombreComercial }}</td>
-                    <td>{{ item.viaAdministrativa }}</td>
-                    <td>{{ item.tipoPresentacion }}</td>
-                    <td>{{ item.cantidadSolicitada }}</td>
-                    <td>{{ item.precioUnitario }}</td>
-                    <td>{{ item.cantidadEntregada }}</td>
-                    <td>{{ item.importe }}</td>
-                    <td>{{ item.fechaVenta }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <b-row>
+              <b-col md="12" class="table-responsive w-100">
+                <b-table striped bordered hover :items="rows" :fields="columns">
+                  <template v-slot:cell(id)="data">
+                    <span>{{ data.item.id }}</span>
+                  </template>
+                  <template v-slot:cell(personalMedicoId)="data">
+                    <span>{{ data.item.personalMedicoId }}</span>
+                  </template>
+                  <template v-slot:cell(recetaId)="data">
+                    <span>{{ data.item.recetaId }}</span>
+                  </template>
+                  <template v-slot:cell(nombreGenerico)="data">
+                    <span>{{ data.item.nombreGenerico }}</span>
+                  </template>
+                  <template v-slot:cell(nombreComercial)="data">
+                    <span>{{ data.item.nombreComercial }}</span>
+                  </template>
+                  <template v-slot:cell(viaAdministrativa)="data">
+                    <span>{{ data.item.viaAdministrativa }}</span>
+                  </template>
+                  <template v-slot:cell(tipoPresentacion)="data">
+                    <span>{{ data.item.tipoPresentacion }}</span>
+                  </template>
+                  <template v-slot:cell(cantidadSolicitada)="data">
+                    <span>{{ data.item.cantidadSolicitada }}</span>
+                  </template>
+                  <template v-slot:cell(precioUnitario)="data">
+                    <span>{{ data.item.precioUnitario }}</span>
+                  </template>
+                  <template v-slot:cell(cantidadEntregada)="data">
+                    <span>{{ data.item.cantidadEntregada }}</span>
+                  </template>
+                  <template v-slot:cell(importe)="data">
+                    <span>{{ data.item.importe }}</span>
+                  </template>
+                  <template v-slot:cell(estatus)="data">
+                    <span>{{ data.item.estatus }}</span>
+                  </template>
+                  <template v-slot:cell(fechaVenta)="data">
+                    <span>{{ data.item.fechaVenta }}</span>
+                  </template>
+                </b-table>
+              </b-col>
+            </b-row>
           </template>
         </iq-card>
       </b-col>
@@ -43,93 +59,85 @@
   </b-container>
 </template>
 
+
 <script>
-import { xray } from '../../config/pluginInit'
 import iqCard from '../../components/xray/cards/iq-card'
+import axios from 'axios';
+import { ref } from 'vue'
+
 
 export default {
   name: 'UiDataTable',
   components: { iqCard },
-  mounted() {
-    xray.index()
-  },
-  data() {
-    return {
-      columns: [
-        { label: 'ID', field: 'id', headerClass: 'text-left' },
-        { label: 'Personal Médico ID', field: 'personalMedicoId', headerClass: 'text-left' },
-        { label: 'Receta ID', field: 'recetaId', headerClass: 'text-left' },
-        { label: 'Nombre Genérico', field: 'nombreGenerico', headerClass: 'text-left' },
-        { label: 'Nombre Comercial', field: 'nombreComercial', headerClass: 'text-left' },
-        { label: 'Vía Administrativa', field: 'viaAdministrativa', headerClass: 'text-left' },
-        { label: 'Tipo de Presentación', field: 'tipoPresentacion', headerClass: 'text-left' },
-        { label: 'Cantidad Solicitada', field: 'cantidadSolicitada', headerClass: 'text-left' },
-        { label: 'Precio Unitario', field: 'precioUnitario', headerClass: 'text-left' },
-        { label: 'Cantidad Entregada', field: 'cantidadEntregada', headerClass: 'text-left' },
-        { label: 'Importe', field: 'importe', headerClass: 'text-left' },
-        { label: 'Fecha de la Venta', field: 'fechaVenta', headerClass: 'text-left' }
-      ],
-      rows: []
-    }
-  },
-  created() {
-    this.generateData();
-  },
-  methods: {
-    generateData() {
-      const viasAdministracion = [
-        'Oral', 'Intravenoso', 'Rectal', 'Cutáneo', 'Subcutáneo',
-        'Oftálmico', 'Ótica', 'Nasal', 'Tópica', 'Parental', 'Vaginal'
-      ];
-      const tiposPresentacion = [
-        'Grageas', 'Suspensión', 'Pomada', 'Jabón', 'Polvo', 'Supositorios',
-        'Solución', 'Cápsulas', 'Parche', 'Jarabe', 'Gotas', 'Tabletas'
-      ];
-      for (let i = 1; i <= 50; i++) {
-        const randomId = Math.random();
-        const viaAdministrativa = viasAdministracion[Math.floor(Math.random() * viasAdministracion.length)];
-        const tipoPresentacion = tiposPresentacion[Math.floor(Math.random() * tiposPresentacion.length)];
-        if (randomId < 0.5) {
-          this.rows.push({
-            id: i,
-            personalMedicoId: 'PM' + i,
-            recetaId: null,
-            nombreGenerico: 'Medicamento ' + i,
-            nombreComercial: 'Comercial ' + i,
-            viaAdministrativa: viaAdministrativa,
-            tipoPresentacion: tipoPresentacion,
-            cantidadSolicitada: this.getRandomInt(1, 20),
-            precioUnitario: '$' + (Math.random() * (100 - 1) + 1).toFixed(2),
-            cantidadEntregada: this.getRandomInt(1, 20),
-            importe: '$' + (Math.random() * (1000 - 10) + 10).toFixed(2),
-            fechaVenta: this.getRandomDate()
-          });
-        } else {
-          this.rows.push({
-            id: i,
-            personalMedicoId: null,
-            recetaId: 100 + i,
-            nombreGenerico: 'Medicamento ' + i,
-            nombreComercial: 'Comercial ' + i,
-            viaAdministrativa: viaAdministrativa,
-            tipoPresentacion: tipoPresentacion,
-            cantidadSolicitada: this.getRandomInt(1, 20),
-            precioUnitario: '$' + (Math.random() * (100 - 1) + 1).toFixed(2),
-            cantidadEntregada: this.getRandomInt(1, 20),
-            importe: '$' + (Math.random() * (1000 - 10) + 10).toFixed(2),
-            fechaVenta: this.getRandomDate()
-          });
+  setup() {
+
+    const columns = [
+      { key: 'id', label: 'ID', field: 'id', headerClass: 'text-left' },
+      { key: 'personalMedicoId', label: 'Personal Médico ID', field: 'personalMedicoId', headerClass: 'text-left' },
+      { key: 'recetaId', label: 'Receta ID', field: 'recetaId', headerClass: 'text-left' },
+      { key: 'nombreGenerico', label: 'Nombre Genérico', field: 'nombreGenerico', headerClass: 'text-left' },
+      { key: 'nombreComercial', label: 'Nombre Comercial', field: 'nombreComercial', headerClass: 'text-left' },
+      { key: 'viaAdministrativa', label: 'Vía Administrativa', field: 'viaAdministrativa', headerClass: 'text-left' },
+      { key: 'tipoPresentacion', label: 'Tipo de Presentación', field: 'tipoPresentacion', headerClass: 'text-left' },
+      { key: 'cantidadSolicitada', label: 'Cantidad Solicitada', field: 'cantidadSolicitada', headerClass: 'text-left' },
+      { key: 'precioUnitario', label: 'Precio Unitario', field: 'precioUnitario', headerClass: 'text-left' },
+      { key: 'cantidadEntregada', label: 'Cantidad Entregada', field: 'cantidadEntregada', headerClass: 'text-left' },
+      { key: 'importe', label: 'Importe', field: 'importe', headerClass: 'text-left' },
+      { key: 'estatus', label: 'Estatus', field: 'estatus', headerClass: 'text-left' },
+      { key: 'fechaVenta', label: 'Fecha de la Venta', field: 'fechaVenta', headerClass: 'text-left' }
+    ]
+    const rows = ref([])
+
+    const fetchData = async () => {
+      try {
+        const response1 = await axios.get('http://127.0.0.1:8000/hospital/api/v1c_dispensacion_medicamentos/')
+        const response2 = await axios.get('http://127.0.0.1:8000/hospital/api/v1c_detalle_dispensacion/')
+
+        // Mapear los datos de la primera API
+        const data1 = response1.data.map(item => ({
+          personalMedicoId : item.Personal_Medico_ID ,
+          recetaId : item.Receta_ID,
+          cantidadSolicitada : item.Total_Medicamentos_Solicitados,
+          cantidadEntregada: item.Total_Medicamentos_Entregados,
+          estatus: item.Estatus
+
+        }))
+
+        // Mapear los datos de la segunda API
+        const data2 = response2.data.map(item => ({
+          id: item.Dispensacion_ID
+        }))
+
+        // Combina los datos de ambas API
+        const combinedData = data1.map(item1 => {
+          const correspondingItem = data2.find(item2 => item1.id === item2.id)
+          return correspondingItem ? { ...item1, ...correspondingItem } : item1
+        })
+
+        // Asignar el conjunto combinado de datos a rows
+        rows.value = combinedData
+      } catch (error) {
+        console.error('Error fetching data:', error)
       }
     }
-  },
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
-  getRandomDate() {
-    const start = new Date(2019, 0, 1);
-    const end = new Date();
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString().split('T')[0];
+
+    fetchData()
+
+    const remove = (item) => {
+      let index = rows.value.indexOf(item)
+      rows.value.splice(index, 1)
+    }
+
+
+
+    const formatDate = (date) => {
+      const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
+      return new Date(date).toLocaleDateString('es-ES', options)
+    }
+
+    return { columns, rows, remove, formatDate }
   }
 }
-}
+
+
 </script>
