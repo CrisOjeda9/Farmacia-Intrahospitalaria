5b725c3da2081bc0b6dbc0f621acf5eb54e34b11
Myriam Valderrabano <210467@utxicotepec.edu.mx>
2024-04-17 00:57:40 -0600

    Consultas Parte 2

--- Database/SQL/Consultas_Farmacia.sql
+++ Database/SQL/Consultas_Farmacia.sql
@@ -1,4 +1,4 @@
-use bd_hospital_farmacia;
+use bd_hospital_210467;
 
 SHOW PROCEDURE STATUS WHERE Db = 'bd_hospital_farmacia';
 
@@ -19,16 +19,21 @@ CALL sp_insertar_lotes(10);
 -- El id de la receta, lo deje null, ya que no puedo hacer un registro de prueba en receta, 
 -- porque la tabla contiene citaId y no existe. POR ESE CASO EL ATRIBUTO RECETA_ID SE LE QUITO NN
 -- Lo que corresponde al total aun falta, estoy ocupando por el momento cantidades aleatorias, usando la fn genera_cantidad aplicado en sp_lotes...
-CALL sp_insertar_dispensacion_medicamentos(4); -- INSERTA EN DISPENSACION_MEDICAMENTOS
+CALL sp_insertar_dispensacion_medicamentos(1,8); -- INSERTA EN DISPENSACION_MEDICAMENTOS
 -- SELECT sp_insertar_dispensacion(2); aun no funciona 
 
-
+CALL sp_insertar_dispensacion_medicamentos(1, 3);
 select*from dispensacion_medicamentos;
+select*from detalles_dispensacion;
+
+
 select*from lotes_medicamentos;
 select*from detalle_lotes;
 
 
 
 -- SP, TRIGGERS DE LOTES, SP Y TRIGGERS DE LA TABLA DISPENSACION_MEDICAMENTOS
+
+select fn_calcular_cantidad_por_unidad (4);
 
 
