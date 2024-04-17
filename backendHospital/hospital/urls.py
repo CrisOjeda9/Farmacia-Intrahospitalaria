418c4915e10edb8f329a54c9d17a4b66abe9d0d9
Daniela-AG15 <danaguilar1512@gmail.com>
2024-04-16 23:32:09 -0600

    Modificacion del Archivo urls

--- backendHospital/hospital/urls.py
+++ backendHospital/hospital/urls.py
@@ -14,13 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from rest_framework.documentation import include_docs_urls
 
+from django.urls import path,include
+from rest_framework import routers
+from api import views
+
+# Define el enrutador
+router = routers.DefaultRouter()
+
+# Registra cada vista de conjunto de vistas con el enrutador
+router.register(r'areas_medicas', views.AreasMedicasViewSet)
+router.register(r'bitacora', views.BitacoraViewSet)
+router.register(r'citas', views.CitasViewSet)
+router.register(r'departamentos', views.DepartamentosViewSet)
+router.register(r'detalle_dispensacion_relacion', views.DetalleDispensacionRelacionViewSet)
+router.register(r'detalle_lotes', views.DetalleLotesViewSet)
+router.register(r'detalles_dispensacion', views.DetallesDispensacionViewSet)
+router.register(r'dispensacion_medicamentos', views.DispensacionMedicamentosViewSet)
+router.register(r'lotes_medicamentos', views.LotesMedicamentosViewSet)
+router.register(r'medicamentos', views.MedicamentosViewSet)
+router.register(r'pacientes', views.PacientesViewSet)
+router.register(r'personal_medico', views.PersonalMedicoViewSet)
+router.register(r'personas', views.PersonasViewSet)
+router.register(r'receta_medica', views.RecetaMedicaViewSet)
+router.register(r'receta_medica_detalle', views.RecetaMedicaDetalleViewSet)
+
+# Define las URL con el prefijo 'api/v1/' e incluye las URLs generadas por el enrutador
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('hospital/', include('api.urls')),
-    path('docs/', include_docs_urls(title="hospital API'S")),
+	path('api/v1',include(router.urls))
 ]
+
+
