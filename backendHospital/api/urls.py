a04ae9a0a4e27979c6598da3f391e4533d27c6fb
Daniela-AG15 <danaguilar1512@gmail.com>
2024-04-16 23:36:37 -0600

    Modificacion urls b

--- backendHospital/api/urls.py
+++ backendHospital/api/urls.py
@@ -2,13 +2,29 @@ from django.urls import path,include
 from rest_framework import routers
 from api import views
 
+# Define el enrutador
 router = routers.DefaultRouter()
-router.register(r'c_dispensacion_medicamentos', views.c_dispensacionViewSet)
-router.register(r'c_detalles_dispensacion', views.c_detalles_dispensacionViewSet)
-router.register(r'c_lotes_medicamentos', views.c_lotes_medicamentoViewSet)
-router.register(r'c_detalles_lotes', views.c_detalles_lotes_medicamentoViewSet)
 
+# Registra cada vista de conjunto de vistas con el enrutador
+router.register(r'areas_medicas', views.AreasMedicasViewSet)
+router.register(r'bitacora', views.BitacoraViewSet)
+router.register(r'citas', views.CitasViewSet)
+router.register(r'departamentos', views.DepartamentosViewSet)
+router.register(r'detalle_dispensacion_relacion', views.DetalleDispensacionRelacionViewSet)
+router.register(r'detalle_lotes', views.DetalleLotesViewSet)
+router.register(r'detalles_dispensacion', views.DetallesDispensacionViewSet)
+router.register(r'dispensacion_medicamentos', views.DispensacionMedicamentosViewSet)
+router.register(r'lotes_medicamentos', views.LotesMedicamentosViewSet)
+router.register(r'medicamentos', views.MedicamentosViewSet)
+router.register(r'pacientes', views.PacientesViewSet)
+router.register(r'personal_medico', views.PersonalMedicoViewSet)
+router.register(r'personas', views.PersonasViewSet)
+router.register(r'receta_medica', views.RecetaMedicaViewSet)
+router.register(r'receta_medica_detalle', views.RecetaMedicaDetalleViewSet)
+
+# Define las URL con el prefijo 'api/v1/' e incluye las URLs generadas por el enrutador
 urlpatterns = [
 	path('api/v1',include(router.urls))
 ]
+
 
