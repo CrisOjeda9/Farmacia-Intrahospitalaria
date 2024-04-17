a252a052cc6097f7d65f5b6629b51d83f9a80dd0
Daniela-AG15 <danaguilar1512@gmail.com>
2024-04-16 23:56:26 -0600

    Modificacion en el archivo vistas

--- backendHospital/api/views.py
+++ backendHospital/api/views.py
@@ -1,19 +1,78 @@
 from rest_framework import viewsets
-from .models import c_dispensacion_medicamentos,c_detalles_dispensacion,c_lotes_medicamentos,c_detalles_lotes
-from .serializer import c_dispensacionSerializer,c_detalles_dispensacionSerializer,c_lotesSerializer,c_detalles_lotesSerializer
+from .models import AreasMedicas, Bitacora, Citas, Departamentos, DetalleDispensacionRelacion, DetalleLotes, DetallesDispensacion, DispensacionMedicamentos, LotesMedicamentos, Medicamentos, Pacientes, PersonalMedico, Personas, RecetaMedica, RecetaMedicaDetalle
+from .serializer import AreasMedicasSerializer, BitacoraSerializer, CitasSerializer, DepartamentosSerializer, DetalleDispensacionRelacionSerializer, DetalleLotesSerializer, DetallesDispensacionSerializer, DispensacionMedicamentosSerializer, LotesMedicamentosSerializer, MedicamentosSerializer, PacientesSerializer, PersonalMedicoSerializer, PersonasSerializer, RecetaMedicaSerializer, RecetaMedicaDetalleSerializer
 
-class c_dispensacionViewSet(viewsets.ModelViewSet):
-	queryset = c_dispensacion_medicamentos.objects.all()
-	serializer_class = c_dispensacionSerializer
 
-class c_detalles_dispensacionViewSet(viewsets.ModelViewSet):
-	queryset = c_detalles_dispensacion.objects.all()
-	serializer_class = c_detalles_dispensacionSerializer
+class AreasMedicasViewSet(viewsets.ModelViewSet):
+    queryset = AreasMedicas.objects.all()
+    serializer_class = AreasMedicasSerializer
 
-class c_lotes_medicamentoViewSet(viewsets.ModelViewSet):
-	queryset = c_lotes_medicamentos.objects.all()
-	serializer_class = c_lotesSerializer
 
-class c_detalles_lotes_medicamentoViewSet(viewsets.ModelViewSet):
-	queryset = c_detalles_lotes.objects.all()
-	serializer_class = c_detalles_lotesSerializer
+class BitacoraViewSet(viewsets.ModelViewSet):
+    queryset = Bitacora.objects.all()
+    serializer_class = BitacoraSerializer
+
+
+class CitasViewSet(viewsets.ModelViewSet):
+    queryset = Citas.objects.all()
+    serializer_class = CitasSerializer
+
+
+class DepartamentosViewSet(viewsets.ModelViewSet):
+    queryset = Departamentos.objects.all()
+    serializer_class = DepartamentosSerializer
+
+
+class DetalleDispensacionRelacionViewSet(viewsets.ModelViewSet):
+    queryset = DetalleDispensacionRelacion.objects.all()
+    serializer_class = DetalleDispensacionRelacionSerializer
+
+
+class DetalleLotesViewSet(viewsets.ModelViewSet):
+    queryset = DetalleLotes.objects.all()
+    serializer_class = DetalleLotesSerializer
+
+
+class DetallesDispensacionViewSet(viewsets.ModelViewSet):
+    queryset = DetallesDispensacion.objects.all()
+    serializer_class = DetallesDispensacionSerializer
+
+
+class DispensacionMedicamentosViewSet(viewsets.ModelViewSet):
+    queryset = DispensacionMedicamentos.objects.all()
+    serializer_class = DispensacionMedicamentosSerializer
+
+
+class LotesMedicamentosViewSet(viewsets.ModelViewSet):
+    queryset = LotesMedicamentos.objects.all()
+    serializer_class = LotesMedicamentosSerializer
+
+
+class MedicamentosViewSet(viewsets.ModelViewSet):
+    queryset = Medicamentos.objects.all()
+    serializer_class = MedicamentosSerializer
+
+
+class PacientesViewSet(viewsets.ModelViewSet):
+    queryset = Pacientes.objects.all()
+    serializer_class = PacientesSerializer
+
+
+class PersonalMedicoViewSet(viewsets.ModelViewSet):
+    queryset = PersonalMedico.objects.all()
+    serializer_class = PersonalMedicoSerializer
+
+
+class PersonasViewSet(viewsets.ModelViewSet):
+    queryset = Personas.objects.all()
+    serializer_class = PersonasSerializer
+
+
+class RecetaMedicaViewSet(viewsets.ModelViewSet):
+    queryset = RecetaMedica.objects.all()
+    serializer_class = RecetaMedicaSerializer
+
+
+class RecetaMedicaDetalleViewSet(viewsets.ModelViewSet):
+    queryset = RecetaMedicaDetalle.objects.all()
+    serializer_class = RecetaMedicaDetalleSerializer
